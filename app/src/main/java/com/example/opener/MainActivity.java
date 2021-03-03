da51f95f1f7ffdf480d495289d5a2e5f83585edd
aparhomenko <aparhomenko@akber-soft.ru>
2021-03-03 18:25:02 +0500

    DB test

--- app/src/main/java/com/example/opener/MainActivity.java
+++ app/src/main/java/com/example/opener/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.opener;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Service;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.security.Provider;
 
@@ -23,31 +28,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int permissionStatusPhone = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+
+        SQLiteDatabase myDB =
+                openOrCreateDatabase("my.db", MODE_PRIVATE, null);
+
+        myDB.execSQL(
+                "CREATE TABLE IF NOT EXISTS tel (number INT, btn INT)"
+        );
+
+        ContentValues row1 = new ContentValues();
+        row1.put("number", 666);
+        row1.put("btn",1);
+        myDB.insert("tel", null, row1);
+        myDB.close();
+
+
+
 
         Button btn1 = findViewById(R.id.button);
         Button btn2 = findViewById(R.id.button2);
         Button btn3 = findViewById(R.id.button3);
-        ImageView btnSettings = findViewById(R.id.buttonSettings);
         btn3.setText("3");
 
-        if (permissionStatusPhone == PackageManager.PERMISSION_DENIED){
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED){
             ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CALL_PHONE}, 1);
         }
 
-        btnSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Settings.class);
-                startActivity(intent);
-            }
-        });
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                     //startService(new Intent(MainActivity.this, openerService.class));
-                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:+7"+tel1)));}
+                SQLiteDatabase myDB =
+                        openOrCreateDatabase("my.db", MODE_PRIVATE, null);
+                Cursor myCursor =
+                        myDB.rawQuery("select * from tel;", null);
+
+
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:+7"+myCursor.getInt(0))));
+                myCursor.close();
+            }
 
         });
 

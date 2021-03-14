ec3679933528d6590e4bf627766745241cc8bf5a
Anton <parkhanton@gmail.com>
2021-03-14 19:24:55 +0500

    Something done

--- app/src/main/java/com/example/opener/MainActivity.java
+++ app/src/main/java/com/example/opener/MainActivity.java
@@ -6,9 +6,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Service;
+import android.app.VoiceInteractor;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -18,8 +20,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
@@ -33,15 +37,22 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.security.Provider;
+import java.util.Arrays;
+
+import static android.Manifest.permission.CALL_PHONE;
 
 public class MainActivity extends AppCompatActivity {
     boolean touched;
     SQLiteDatabase myDB ;
     Button btn1, btn2, btn3 ;
     final int PICK_CONTACT = 1;
+    public static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1, PERMISSIONS_REQUEST_CALL_PHONE = 2;
+    public static int COPY_CONTACT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,13 +69,13 @@ public class MainActivity extends AppCompatActivity {
 
         myDB =openOrCreateDatabase("new.db", MODE_PRIVATE, null);
         myDB.execSQL(
-                "CREATE TABLE IF NOT EXISTS tel2 (number CHAR(25), btn CHAR(25), btnnum CHAR(3))"
+                "CREATE TABLE IF NOT EXISTS tel2 (number STRING, btn STRING, btnnum CHAR(3))"
         );
 
         loadFromBD();
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CALL_PHONE}, 1);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, CALL_PHONE) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {CALL_PHONE}, PERMISSIONS_REQUEST_CALL_PHONE);
         }
 
         settBtn.setOnClickListener(new View.OnClickListener() {
@@ -168,19 +179,22 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(View v) {
                                 if (nameText.getEditText().getText().length()<1){
+                                    YoYo.with(Techniques.Shake).duration(700).playOn(nameText);
                                     nameText.setError("Поле не может быть пустым");
                                     if (numberText.getEditText().getText().length()<1){
+                                        YoYo.with(Techniques.Shake).duration(700).playOn(numberText);
                                         numberText.setError("Поле не может быть пустым");
                                     }
                                 }
                                 else if (numberText.getEditText().getText().length()<1){
+                                    YoYo.with(Techniques.Shake).duration(700).playOn(numberText);
                                     numberText.setError("Поле не может быть пустым");
                                 }
                                 else if (nameText.getEditText().getText().length()>25){
+                                    YoYo.with(Techniques.Shake).duration(700).playOn(nameText);
                                     nameText.setError("Не больше 25 символов");
                                 }
                                 else{
-
                                     if (cursor.getCount() < 3){
                                         String number = numberText.getEditText().getText().toString();
                                         String name = nameText.getEditText().getText().toString();
@@ -188,6 +202,8 @@ public class MainActivity extends AppCompatActivity {
                                         dialog.cancel();
                                     }
                                     else {
+                                        YoYo.with(Techniques.Shake).duration(700).playOn(nameText);
+                                        YoYo.with(Techniques.Shake).duration(700).playOn(numberText);
                                         nameText.setError("Максимум 3 кнопки на экране");
                                         numberText.setError("Удалите несколько, что-бы продолжить");
                                     }
@@ -204,12 +220,12 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(View v) {
                                 if (cursor.getCount() < 3) {
-                                    Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-                                    startActivityForResult(intent, PICK_CONTACT);
+                                    requestContactPermission();
                                     dialog.cancel();
                                 }
                                 else
                                 {
+                                    YoYo.with(Techniques.Shake).duration(700).playOn(contactBtn);
                                     nameText.setError("Максимум 3 кнопки на экране");
                                     numberText.setError("Удалите несколько, что-бы продолжить");
                                 }
@@ -217,6 +233,7 @@ public class MainActivity extends AppCompatActivity {
                         });
                         cursor.close();
                     }
+
                 });
                 alertDialog.show();
             }
@@ -357,19 +374,70 @@ public class MainActivity extends AppCompatActivity {
                         String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
                         int hasPhoneNumber = Integer.parseInt(c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
                         if (hasPhoneNumber > 0) {
-                            //This is to read multiple phone numbers associated with the same contact
                             String phoneNumber = null;
                             Cursor phoneCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{c.getString(c.getColumnIndex(ContactsContract.Contacts._ID))}, null);
                             while (phoneCursor.moveToNext()) {
                                 phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                             }
                             phoneCursor.close();
-                            String parsedNumber = phoneNumber.replaceAll("[^0-9]","");
-                            createBtn(parsedNumber,name);
+
+                            createBtn(phoneNumber, name);
                         //Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_SHORT).show();
                     }
                 }
                 break;
+            }
+        }
+    }
+
+    public void requestContactPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        android.Manifest.permission.READ_CONTACTS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Для продолжения нужен доступ к контактам.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Пожалуйста, разрешите доступ к контакам.");
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {android.Manifest.permission.READ_CONTACTS}
+                                    , PERMISSIONS_REQUEST_READ_CONTACTS);
+                        }
+                    });
+                    builder.show();
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{android.Manifest.permission.READ_CONTACTS},
+                            PERMISSIONS_REQUEST_READ_CONTACTS);
+                }
+            } else if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, PICK_CONTACT);
+            }
+        } else {
+            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intent, PICK_CONTACT);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                    startActivityForResult(intent, PICK_CONTACT);
+                } else {
+                    Toast.makeText(this, "Вы отклонили запрос на доступ к контактам", Toast.LENGTH_LONG).show();
+                }
+                return;
             }
         }
     }
@@ -439,45 +507,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void createBtn(String number, String name){
-        Cursor cursor = myDB.rawQuery("select * from tel2",null);
-        cursor.moveToFirst();
-        if (cursor.getCount() == 0) {
-            ContentValues row1 = new ContentValues();
-            row1.put("number", number);
-            row1.put("btn", name);
-            row1.put("btnnum", "1");
-            myDB.insert("tel2", null, row1);
-            cursor.close();
-            btn1.setVisibility(View.VISIBLE);
-            btn1.setText(name);
-        }
-        else {
-            cursor.moveToNext();
-            if (cursor.getCount() == 1){
+    void createBtn(String number, String name) {
+        Cursor cursor1 = myDB.rawQuery("select * from tel2", null);
+        cursor1.moveToFirst();
+        if (cursor1.getCount() == 0) {
                 ContentValues row1 = new ContentValues();
                 row1.put("number", number);
                 row1.put("btn", name);
-                row1.put("btnnum", "2");
+                row1.put("btnnum", "1");
                 myDB.insert("tel2", null, row1);
-                cursor.close();
-                btn2.setVisibility(View.VISIBLE);
-                btn2.setText(name);
-            }
-            else {
-                cursor.moveToNext();
-                if (cursor.getCount() == 2){
+                cursor1.close();
+                btn1.setVisibility(View.VISIBLE);
+                btn1.setText(name);
+        } else {
+            cursor1.moveToNext();
+            if (cursor1.getCount() == 1) {
                     ContentValues row1 = new ContentValues();
                     row1.put("number", number);
                     row1.put("btn", name);
-                    row1.put("btnnum", "3");
+                    row1.put("btnnum", "2");
                     myDB.insert("tel2", null, row1);
-                    cursor.close();
-                    btn3.setVisibility(View.VISIBLE);
-                    btn3.setText(name);
+                    cursor1.close();
+                    btn2.setVisibility(View.VISIBLE);
+                    btn2.setText(name);
+            } else {
+                cursor1.moveToNext();
+                if (cursor1.getCount() == 2) {
+                        ContentValues row1 = new ContentValues();
+                        row1.put("number", number);
+                        row1.put("btn", name);
+                        row1.put("btnnum", "3");
+                        myDB.insert("tel2", null, row1);
+                        cursor1.close();
+                        btn3.setVisibility(View.VISIBLE);btn3.setText(name);
+                    }
                 }
             }
-        }
     }
 
     void callToOpen (String text){
